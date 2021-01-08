fcef452ffba854464793d5212d7ef018dc9157f6
bindev-bit <bindev.bit@gmail.com>
2021-01-08 09:37:59 +0700

    jahitin web apps with react

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/styles/tailwind.css";
@@ -18,18 +18,19 @@ import Index from "views/Index.js";
 import Product from "views/Product.js";
 
 ReactDOM.render(
-  <HashRouter basename={process.env.PUBLIC_URL}>
+  <BrowserRouter>
     <Switch>
       {/* add routes with layouts */}
       <Route path="/admin" component={Admin} />
       <Route path="/auth" component={Auth} />
       {/* add routes without layouts */}
-      <Route path="landing" exact component={Landing} />
+      <Route path="/landing" exact component={Landing} />
       <Route path="/profile" exact component={Profile} />
       <Route path="/product" exact component={Product} />
       <Route path="/" exact component={Index} />
       {/* add redirect for first page */}
+      <Redirect from="*" to="/" />
     </Switch>
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
